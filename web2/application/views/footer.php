b12a0acbcd0d5ebe970e541504482fb397982ead
kaitookita <kaitookita2@gmail.com>
2016-09-04 19:16:32 +0700

    test codeigniter with github
    testcodeigniter

---
+++ web2/application/views/footer.php
@@ -0,0 +1,25 @@
+
+
+
+<!-- 	<footer>
+	<div class="container">
+		<div class="row">
+            <div class="col-lg-12">
+                <p>Copyright &copy; Your Website 2014</p>
+            </div>
+        </div>
+    </div>
+</footer> -->
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="public/js/bootstrap.min.js"></script>
+
+    <!-- Lightbox -->
+	<script src="public/js/custom/lightbox.min.js"></script>
+
+  </body>
+</html>
